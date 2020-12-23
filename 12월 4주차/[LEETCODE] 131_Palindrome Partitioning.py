1ec683291f9d70d91fd8b98f6717324968d06598
Gayoung <gyyoon4u@naver.com>
2020-12-23 12:01:32 +0900

    1223_Palindrome Partitioning_재귀

---
+++ 12월 4주차/[LEETCODE] 131_Palindrome Partitioning.py
@@ -0,0 +1,21 @@
+# 재귀사용했는데 for word있는 부분에서 어떻게 처리해야할지 시간걸림.
+
+# class Solution:
+def partition(s):
+
+    def go(s, part):
+        if not s:
+            answer.append(part)
+        
+        for i in range(len(s)):
+            if checkPalindrome(s[:i+1]):
+                go(s[i+1:], part+[s[:i+1]])
+
+    def checkPalindrome(s):
+        return s == s[::-1]
+    
+    answer = []
+    go(s, [])
+    return answer
+    
+print(partition("aab"))
